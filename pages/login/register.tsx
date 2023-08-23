8b7a79664367f056eccbab5d351805f4ff76de51
umuta <umutakkemik@gmail.com>
2023-08-23 20:03:10 +0300

    Kayıt alanı hataları giderildi.

--- pages/login/register.tsx
+++ pages/login/register.tsx
@@ -2,16 +2,26 @@ import React, {useState} from "react";
 import Link from "next/link";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios";
-import { notification } from 'antd';
 import { useRouter } from 'next/router';
 import usersData from '../../utils/users.json';
+import {Form, Input, Button, notification, DatePicker, Select} from 'antd';
+import dayjs from "dayjs";
+const { Option } = Select;
 const RegisterPage:React.FC = () => {
+    const dateFormat = 'YYYY/MM/DD';
     const router = useRouter();
 
-    const openNotification = (type) => {
+    const successNotification = (type) => {
         notification[type]({
             message: 'Kayıt',
             description: 'Kayıt Başarılı',
+            duration: 0,
+        });
+    };
+    const errorNotification = (type) => {
+        notification[type]({
+            message: 'Kayıt Başarısız',
+            description: 'Lütfen bilgileri eksiksiz girin!',
             duration: 0,
         });
     };
@@ -19,17 +29,20 @@ const RegisterPage:React.FC = () => {
     const [name, setName] = useState('');
     const [surname, setSurname] = useState('');
     const [gender, setGender] = useState('');
-    const [birtday, setBirtday] = useState('');
+    const [birtday, setBirtday] = useState(null);
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
         try {
-            const newUser = {  id: usersData.users.length + 1,email,name,surname,gender,birtday,password };
-            const response = await axios.post('/api/users', newUser );
-            openNotification('success')
-            router.push("/login")
+            if (email){
+                const newUser = {  id: usersData.users.length + 1,email,name,surname,gender,birtday,password };
+                const response = await axios.post('/api/users', newUser );
+                successNotification('success')
+                router.push("/login")
+            } else {
+                errorNotification('error')
+            }
         } catch (error) {
             console.error('Kullanıcı eklenirken hata oluştu:', error);
         }
@@ -39,45 +52,37 @@ const RegisterPage:React.FC = () => {
             <main
                 style={{backgroundImage: '../img/login-background.jpg', width: '100%', maxWidth: '450px', padding: '30px', marginTop: '50px', margin: 'auto', borderStyle: 'outset', borderRadius: '10px 10px 10px 10px'}}
                 className="form-signin text-center mt-5">
-                <form onSubmit={handleSubmit}>
-                    <h1 className="h3 mb-3 fw-normal">Kayıt Ol</h1>
-
-                    <div style={{padding: '5px'}} className="form-floating">
-                        <input type="text" className="form-control" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="name@example.com"/>
-                        <label htmlFor="floatingInput">Email</label>
-                    </div>
-                    <div style={{padding: '5px'}} className="form-floating">
-                        <input type="text" className="form-control" value={name} onChange={(e)=>setName(e.target.value)} placeholder="name@example.com"/>
-                        <label htmlFor="floatingInput">Ad</label>
-                    </div>
-                    <div style={{padding: '5px'}} className="form-floating">
-                        <input type="text" className="form-control" value={surname} onChange={(e)=>setSurname(e.target.value)} placeholder="name@example.com"/>
-                        <label htmlFor="floatingInput">Soyad</label>
-                    </div>
-                    <div style={{padding: '5px'}} className="form-floating">
-                        <select value={gender} onChange={(e)=>setGender(e.target.value)} className="form-control">
-                            <option selected>Cinsiyet</option>
-                            <option key="1" value="1">Erkek</option>
-                            <option key="2" value="2">kadın</option>
-                        </select>
-                    </div>
-                    <div style={{padding: '5px'}} className="form-floating">
-                        <input type="date" className="form-control" value={birtday} onChange={(e)=>setBirtday(e.target.value)} placeholder="name@example.com"/>
-                        <label htmlFor="floatingInput">Doğum Tarihi</label>
-                    </div>
-                    <div style={{padding: '5px'}} className="form-floating">
-                        <input type="password" className="form-control" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password"/>
-                        <label htmlFor="floatingPassword">Şifre</label>
-                    </div>
-
-                    <div className="mb-3">
-                        <label style={{float: 'right', padding: '5px'}}>
-                            <Link href="/login">Giriş Yap</Link>
-                        </label>
-                    </div>
-                    <button className="w-100 btn btn-lg btn-primary" type="submit">Kayıt Ol</button>
-                    {error && <p style={{ color: 'red' }}>{error}</p>}
-                </form>
+                <Form onFinish={handleSubmit} name="form_item_path" layout="vertical" >
+                        <Form.Item name="Email alanı" label="Email" rules={[{ required: true }]}>
+                            <Input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                        </Form.Item>
+                        <Form.Item name="Ad alanı" label="Ad" rules={[{ required: true }]}>
+                            <Input value={name} onChange={(e)=>setName(e.target.value)} />
+                        </Form.Item>
+                    <Form.Item name="Soyad alanı" label="Soyad" rules={[{ required: true }]}>
+                        <Input value={surname} onChange={(e)=>setSurname(e.target.value)} />
+                    </Form.Item>
+                    <Form.Item name="Cinsiyet alanı" label="Cinsiyet" rules={[{ required: true }]}>
+                        <Select
+                            placeholder=""
+                            value={gender}
+                            onChange={(value) => setGender(value)}
+                            allowClear
+                        >
+                            <Option value="1">Erkek</Option>
+                            <Option value="2">Kadın</Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item name="Doğum tarihi alanı" label="Doğum Tarihi" rules={[{ required: true }]}>
+                        <DatePicker value={birtday} onChange={(date, dateString) => setBirtday(dateString)} className="form-control" format={dateFormat} />
+                    </Form.Item>
+                    <Form.Item name="Şifre alanı" label="Ad" rules={[{ required: true }]}>
+                        <Input value={password} onChange={(e)=>setPassword(e.target.value)} />
+                    </Form.Item>
+                    <Button style={{height:'50px'}} className="w-100 btn btn-lg btn-primary" type="primary" htmlType="submit">
+                        Kayıt Ol
+                    </Button>
+                </Form>
             </main>
         </div>
     )
