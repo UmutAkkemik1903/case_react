2c1e140fb7a7b50e27fa71bad6e1fb61c60abea8
umuta <umutakkemik@gmail.com>
2023-08-23 11:07:18 +0300

    Proje oluşturuldu.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">Hakkımızda</Link>
+    </p>
+  </Layout>
+)
+
+export default IndexPage
